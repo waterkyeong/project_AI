ac343d16978217198f553f05f96fb40e9b5bfa32
2eemj000 <167741157+2eemj000@users.noreply.github.com>
2024-08-30 08:44:26 +0900

    Footer 수정

--- src/Compo/Footer.js
+++ src/Compo/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
+    <footer className='w-full flex flex-shrink-0 justify-center text-align-center items-center h-20 mt-8 bg-slate-400 text-purple-50 font-mono'>
       ⓒ 2024 SMARTSHIP, All rights reserved.
     </footer>
   );
